b92180f9180e7180a307ff56055aaf099e1a617f
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-06-17 12:52:12 +0530

    Create Manmohan Loves Patterns- II.cpp

---
+++ Manmohan Loves Patterns- II.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+
+int n,i,j,val;
+cin>>n;
+
+
+cout<<1<<endl;
+for(i=1;i<n;i++){
+        for(j=0;j<=i;j++){
+           cout<<i<<" ";
+        }
+        cout<<endl;
+}
+
+
+cout<<1<<endl;
+
+return 0;
+}
