09ffbfe79d04e97740aeb4e6b0b3f5eb3ca06e34
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-06-18 12:09:07 +0530

    Create Unique Number - I.cpp

---
+++ Unique Number - I.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int n;
+	cin>>n;
+
+	int no;
+	int ans=0;
+	for(int i=0;i<n;i++){
+		cin>>no;
+		ans=ans^no;
+
+	}
+	cout<<ans<<endl;
+	return 0;
+}
