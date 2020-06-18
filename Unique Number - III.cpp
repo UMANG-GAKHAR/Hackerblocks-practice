f58427de61dfab838c3981dcfc27ee14a6b951b4
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-06-18 12:16:03 +0530

    Create Unique Number - III.cpp

---
+++ Unique Number - III.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+	int cnt[64]={0};
+	int n,no;
+	cin>>n;
+
+	for(int i=0;i<n;i++){
+		cin>>no;
+		//update cnt array
+		int j=0;
+		while(no>0){
+			int last_bit= (no&1);
+			cnt[j] +=last_bit;
+			j++;
+			no=no>>1;
+		}
+
+	}
+//iterate over the arraaay
+int p=1;
+int ans=0;
+for(int i=0;i<64;i++){
+	cnt[i] %=3;
+	ans+=(cnt[i]*p);
+	p=p<<1;
+
+}
+//print array
+cout<<ans<<endl;
+
+
+	return 0;
+}
+
+	
