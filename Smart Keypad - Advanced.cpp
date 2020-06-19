c8dd68f9c994682f3d35b8c7d515f25f4d2ea053
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-06-19 11:46:27 +0530

    Create Smart Keypad - Advanced.cpp

---
+++ Smart Keypad - Advanced.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> table = {" ", ".+@$", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+vector<string> searchIn = {
+    "prateek", "sneha", "deepak", "arnav", "shikha", "palak",
+    "utkarsh", "divyam", "vidhi", "sparsh", "akku"};
+
+void phoneKeypad(string inp, string out)
+{
+    //Base case
+    if (inp.size() == 0)
+    {
+        for (string ss : searchIn)
+        {
+            if (ss.find(out) != ss.npos)
+            {
+                cout << ss << endl;
+            }
+        }
+        return;
+    }
+
+    //Rec case
+    char ch = inp[0];
+    string ros = inp.substr(1);
+    string code = table[ch - '0'];
+
+    for (int i = 0; i < code.length(); i++)
+        phoneKeypad(ros, out + code[i]);
+}
+
+int main()
+{
+    string input;
+    cin >> input;
+
+    string output;
+    phoneKeypad(input, output);
+    return 0;
+}
