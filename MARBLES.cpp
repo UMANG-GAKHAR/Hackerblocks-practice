f0876f8d652742e801ea93a8e92cd1c4e6aa92db
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-06-23 11:37:12 +0530

    Create MARBLES.cpp

---
+++ MARBLES.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+int main() {
+    int testCases;
+    cin >> testCases;
+    while (testCases--) {
+        ll n,r;
+        cin >> n >> r;
+        if(n-r < r-1) {
+            r=n-r+1;
+        }
+
+        if(r == 1) {
+            cout << 1 << endl;
+            continue;
+        }
+
+        //Ans = (n-1)!/((r-1)! * (n-r)!)
+
+        ll ans = 1;
+        for(ll i=n-1; i>=n-r+1;i--) {
+            ans = ans*i;
+            ans = ans/(n-i);
+        }
+
+        cout << ans << endl;
+    }
+
+
+
+    return 0;
+}
