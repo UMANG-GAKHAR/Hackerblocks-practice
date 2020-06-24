75a8bb44945f778724114091ea7831b44dfd778a
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-06-24 21:27:28 +0530

    Create 0-1 Knapsack.cpp

---
+++ 0-1 Knapsack.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+ll dp[1005][1005];
+
+//Recursive Approach
+ll knapsack(ll n, ll w, ll *weights, ll *prices)
+{
+    //Base case
+    if (n == 0 || w == 0)
+    {
+        return 0;
+    }
+
+    if (dp[n][w] != -1)
+    {
+        return dp[n][w];
+    }
+
+    ll include = 0;
+    ll exclude = 0;
+
+    if (weights[n - 1] <= w)
+    {
+        //Include the current index element
+        include = prices[n - 1] + knapsack(n - 1, w - weights[n - 1], weights, prices);
+    }
+
+    //Exclude the current element
+    exclude = knapsack(n - 1, w, weights, prices);
+
+    dp[n][w] = max(include, exclude);
+
+    return dp[n][w];
+}
+
+int main()
+{
+    ll n, w;
+    cin >> n >> w;
+    ll weights[1005] = {0};
+    ll prices[1005] = {0};
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> weights[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> prices[i];
+    }
+
+    memset(dp, -1, sizeof(dp));
+
+    cout << knapsack(n, w, weights, prices);
+
+    return 0;
+}
