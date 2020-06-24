2bc5c62f2e653e11ac98b1493d306a3dfa31eb84
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-06-24 21:05:41 +0530

    Create Strings-Remove Duplicates.cpp

---
+++ Strings-Remove Duplicates.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string removeDuplicate(string s)
+{
+    if (s.size() <= 1)
+    {
+        return s;
+    }
+
+    string ros = removeDuplicate(s.substr(1));
+
+    if (s[0] == ros[0])
+    {
+        return ros;
+    }
+
+    return s[0] + ros;
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    cout << removeDuplicate(s) << endl;
+
+    return 0;
+}
