d56c0d8fd6cbb7444a94ddda4eabfecb3386bd28
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-06-24 21:20:38 +0530

    Create Strings-Odd Even Character.cpp

---
+++ Strings-Odd Even Character.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    char str[1001];
+    cin >> str;
+
+    for (int i = 0; str[i] !='\0'; i++) {
+        if(i&1){
+            str[i]=str[i]-1;
+        }else {
+            str[i]=str[i]+1;
+        }
+    }
+
+    cout << str <<endl;
+    return 0;
+}
