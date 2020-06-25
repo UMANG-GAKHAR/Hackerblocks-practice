ca083cf8106e2c26200b88626ef3f2ca705f44bd
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-06-25 12:16:28 +0530

    Create Birthday Paradox.cpp

---
+++ Birthday Paradox.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+    float p, a;
+    int n;
+    cin >> p;
+    if(p!=1)
+    {
+//Finding Probability for a
+    a = log(1/(1-p));
+//Minimum number of people required
+    n = sqrt (2*365*a);
+    }
+//If given probability is one
+    else
+    {
+        n = 365;
+    }
+    cout << n+1;
+
+    return 0;
+}
