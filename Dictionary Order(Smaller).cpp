dfbbd9c0394a31b8d6de5088d21977014188465b
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-06-26 12:21:06 +0530

    Create Dictionary Order(Smaller).cpp

---
+++ Dictionary Order(Smaller).cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string str;
+
+void permute(string inp, string out)
+{
+    //Base case
+    if (inp.size() == 0)
+    {
+        if (out < str)
+        {
+            cout << out << endl;
+        }
+        return;
+    }
+
+    //Recursive case
+    for (int i = 0; i < inp.size(); i++)
+    {
+        string ros = inp.substr(0, i) + inp.substr(i + 1);
+        permute(ros, out + inp[i]);
+    }
+}
+int main()
+{
+    cin >> str;
+
+    string temp = str;
+
+    sort(temp.begin(), temp.end());
+
+    permute(temp, "");
+
+    return 0;
+}
+
