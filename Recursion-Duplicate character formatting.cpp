042b1aed538168978362909972056d5ba08da9d6
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-06-27 13:27:00 +0530

    Create Recursion-Duplicate character formatting.cpp

---
+++ Recursion-Duplicate character formatting.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string duplicateChar(string s)
+{
+    if (s.size() <= 1)
+    {
+        return s;
+    }
+
+    string ros = duplicateChar(s.substr(1));
+
+    if (s[0] == ros[0])
+    {
+        return s[0] + string("*") + ros;
+    }
+
+    return s[0] + ros;
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    cout << duplicateChar(s) << endl;
+
+    return 0;
+}
