65ae6ecd9c6123ae1e22ada3ba264d13c02ad18b
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-06-27 13:27:59 +0530

    Create Recursion-Convert String to Integer.cpp

---
+++ Recursion-Convert String to Integer.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int strToInt(string str, int number)
+{
+    if (str.length() == 0)
+    {
+        return number;
+    }
+    char ch = str[0];
+    number = number + (ch - 48) * (int)pow(10, str.length() - 1);
+    string ros = str.substr(1);
+    return strToInt(ros, number);
+}
+
+int main()
+{
+    string str;
+    cin >> str;
+    int ans = strToInt(str, 0);
+    cout << ans << endl;
+
+    return 0;
+}
