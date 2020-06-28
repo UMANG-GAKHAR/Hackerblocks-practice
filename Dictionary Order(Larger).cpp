1d279d3d6db37f0f0b715e362e6a56e775e0201b
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-06-28 12:10:22 +0530

    Create Dictionary Order(Larger).cpp

---
+++ Dictionary Order(Larger).cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void lexicoLarger(string str, string osf, bool flag, string originalString)
+{
+    if (str.size() == 0)
+    {
+        if (osf > originalString)
+            cout << osf << endl;
+        return;
+    }
+    for (int i = 0; i < str.size(); i++)
+    {
+        string ros = str.substr(0, i) + str.substr(i + 1);
+        char ch = str[i];
+        if (flag)
+        {
+            lexicoLarger(ros, osf + ch, flag, originalString);
+        }
+        else
+        {
+            if (str[i] >= str[0])
+            {
+                lexicoLarger(ros, osf + ch, flag or ch > str[0], originalString);
+            }
+            else if (str[i] < str[0])
+            {
+                // No call
+            }
+        }
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    string str;
+    cin >> str;
+    string orig = str;
+    sort(str.begin(), str.end());
+    lexicoLarger(str, "", false, orig);
+    return 0;
+}
