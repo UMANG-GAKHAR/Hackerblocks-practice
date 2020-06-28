b7e872097095c1744c0f506f6b32c3ab7f9eeebb
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-06-28 12:07:30 +0530

    Create Recursion-Keypad Codes.cpp

---
+++ Recursion-Keypad Codes.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+string keypad[] = {"", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wx", "yz"};
+
+int count = 0;
+
+void keypadCode(string inp, string out, int i)
+{
+    //Base case
+    if (i == inp.size())
+    {
+        cout << out << " ";
+        count++;
+        return;
+    }
+
+    //Rec case
+    int r = inp[i] - '0';
+    for (int k = 0; k < keypad[r].size(); k++)
+    {
+        char ch = keypad[r][k];
+        keypadCode(inp, out + ch, i + 1);
+    }
+}
+int main()
+{
+    string inp;
+    cin >> inp;
+    string output;
+    keypadCode(inp, output, 0);
+    cout << endl << count;
+    return 0;
+}
