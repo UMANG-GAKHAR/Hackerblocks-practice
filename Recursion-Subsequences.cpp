335d7d0ca192402bf1dc3e8eee6a284177e9d4e9
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-06-28 12:09:19 +0530

    Create Recursion-Subsequences.cpp

---
+++ Recursion-Subsequences.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printSubsequences(string inp, string out)
+{
+    if (inp.size() == 0)
+    {
+        cout << out << " ";
+        return;
+    }
+
+    char ch = inp[0];
+    inp = inp.substr(1);
+    printSubsequences(inp, out);
+    printSubsequences(inp, out + ch);
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    printSubsequences(s, "");
+
+    cout << endl;
+
+    int n = (1 << s.size());
+    cout << n << endl;
+    return 0;
+}
