fec0d696130e0987c12cc85f6abe36babb8a697b
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-06-29 20:39:21 +0530

    Create Power(O(logn)).cpp

---
+++ Power(O(logn)).cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+using namespace std;
+
+float power(float x, int y)
+{
+    float temp;
+    if( y == 0)
+       return 1;
+    temp = power(x, y/2);       
+    if (y%2 == 0)
+        return temp*temp;
+    else
+    {
+        if(y > 0)
+            return x*temp*temp;
+        else
+            return (temp*temp)/x;
+    }
+}  
+
+int main() {
+    int num,pw;
+    cin>>num>>pw;
+    cout<<power(num,pw);
+    return 0;
+}
