878e10a982fed06a62ff3533c7a36180be791c45
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-06-30 13:33:35 +0530

    Create PIVOT OF SORTED AND ROTATED ARRAY.cpp

---
+++ PIVOT OF SORTED AND ROTATED ARRAY.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int* getArray(int n){
+    int *arr = new int[n];
+    for(int i = 0; i < n;++i){
+        cin>>arr[i];
+    }
+    return arr;
+}
+
+int findPivot(int *arr,int low,int high){
+    if(low>high){
+        return -1;
+    }
+    else
+    if(low==high){
+        return low;
+    }
+    int mid = (low + high)/2;
+    if(mid < high && arr[mid] > arr[mid+1]){
+        return mid;
+    }
+    if(mid > low && arr[mid] < arr[mid-1]){
+        return mid-1;
+    }
+    if(arr[low] >= arr[mid]){
+        return findPivot(arr,low,mid-1);
+    }
+    return findPivot(arr,mid+1,high);
+}
+
+int main() {
+    int n;
+    cin>>n;
+    int *arr = getArray(n);
+    int pivot = findPivot(arr,0,n-1);
+    cout<<pivot<<endl;
+    return 0;
+}
