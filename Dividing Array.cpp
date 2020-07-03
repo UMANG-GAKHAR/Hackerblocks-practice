1498e4172453e73427ffb12742f1b4faaa390e6a
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-07-03 20:24:24 +0530

    Create Dividing Array.cpp

---
+++ Dividing Array.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define nn 100100
+#define ll long long int
+ 
+using namespace std;
+ 
+ll a[nn];
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        ll mx=0,mn=0;
+        sort(a,a+n);
+        for(int i=0;i<n/2;i++)
+        {
+            mx+=(a[i+n/2]-a[i]);
+            mn+=(a[2*i+1]-a[2*i]);
+        }
+        cout<<mn<<" "<<mx<<endl;
+    }
+    return 0;
+}
