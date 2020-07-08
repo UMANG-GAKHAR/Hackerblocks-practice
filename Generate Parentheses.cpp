a4a55f41050048048ee0672c11213825ad197a37
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-07-08 11:59:22 +0530

    Create Generate Parentheses.cpp

---
+++ Generate Parentheses.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void generateParenthesis(int n, int openB, int closeB, string str) {
+    if(closeB == n) {
+        cout<<str<<endl;
+        return;
+    }
+    if(openB > closeB) {
+        generateParenthesis(n, openB, closeB+1, str+')');
+    }
+    if(openB < n) {
+        generateParenthesis(n, openB+1, closeB, str+'(');
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int n;    cin >> n;
+    generateParenthesis(n, 0, 0, "");
+    return 0;
+}
