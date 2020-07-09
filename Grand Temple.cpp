c0df18e0dc98e8e182b2af104dff2f9c5c1489f1
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-07-09 13:26:52 +0530

    Create Grand Temple.cpp

---
+++ Grand Temple.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main() {
+    ll n;
+    cin >> n;
+    vector<ll> x;
+    vector<ll> y;
+
+    x.push_back(0);
+    y.push_back(0);
+
+    for(ll i=0;i<n;i++){
+        ll a,b;
+        cin >> a >> b;
+
+        x.push_back(a);
+        y.push_back(b);
+    }
+
+    sort(x.begin(),x.end());
+    sort(y.begin(),y.end());
+
+    ll deltaX = 0;
+    for(ll i=1;i<x.size();i++){
+        ll diff = x[i] - x[i-1] - 1 ;
+        deltaX = max(deltaX,diff);
+    }
+
+    ll deltaY = 0;
+    for(ll i=1;i<y.size();i++){
+        ll diff = y[i] - y[i-1] - 1 ;
+        deltaY = max(deltaY,diff);
+    }
+
+    
+    ll maxArea = deltaX * deltaY ;
+    cout << maxArea ;
+
+
+    return 0;
+}
