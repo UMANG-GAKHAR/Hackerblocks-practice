249b23c86fa2548aeeb5e6c96dc9af4d5e3ffbc2
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-07-10 11:54:30 +0530

    Create Longest Increasing subsequence.cpp

---
+++ Longest Increasing subsequence.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+int lis( int arr[], int n )  
+{  
+    int lis[n]; 
+
+    lis[0] = 1;    
+
+    /* Compute optimized LIS values in bottom up manner */
+    for (int i = 1; i < n; i++ )  
+    { 
+        lis[i] = 1; 
+        for (int j = 0; j < i; j++ )   
+            if ( arr[i] > arr[j] && lis[i] < lis[j] + 1)  
+                lis[i] = lis[j] + 1;  
+    } 
+
+    // Return maximum value in lis[] 
+    return *max_element(lis, lis+n); 
+}  
+
+/* Driver program to test above function */
+int main() 
+{ 
+int n; cin>>n;
+    int arr[n] ; 
+for(int i=0;i<n;i++) cin>>arr[i];
+  // int n = sizeof(arr)/sizeof(arr[0]); 
+    printf("%d", 
+           lis( arr, n )); 
+    return 0; 
+} 
