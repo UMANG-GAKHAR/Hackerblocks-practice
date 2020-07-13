e7cfc50df422bfeca38f89c8010afe617b3e9e24
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-07-13 21:13:48 +0530

    Create Balanced Parenthesis.cpp

---
+++ Balanced Parenthesis.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stack>
+#include <cstring>
+using namespace std;
+
+bool balancedParenthesis(string str)
+{
+    stack<char> s;
+    for (int i = 0; i < str.size(); i++)
+    {
+        char c = str[i];
+        if (c == '(' || c == '{' || c == '[')
+        {
+            s.push(c);
+        }
+        else if (c == ')')
+        {
+            if (s.empty() || s.top() != '(')
+            {
+                return false;
+            }
+            s.pop();
+        }
+        else if (c == '}')
+        {
+            if (s.empty() || s.top() != '{')
+            {
+                return false;
+            }
+            s.pop();
+        }
+        else if (c == ']')
+        {
+            if (s.empty() || s.top() != '[')
+            {
+                return false;
+            }
+            s.pop();
+        }
+    }
+    return s.empty();
+}
+int main()
+{
+    string str;
+    cin >> str;
+    bool ans = balancedParenthesis(str);
+    if (ans)
+        cout << "Yes";
+    else
+        cout << "No";
+
+    return 0;
+}
