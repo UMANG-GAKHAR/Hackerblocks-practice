710fdb81fb35f7e10b0190bc2c1e0981e71b61ea
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-07-20 10:32:33 +0530

    Create Maximum Circular Sum.cpp

---
+++ Maximum Circular Sum.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int n,t;
+	cin>>t;int sum=0;
+	while(t--){
+		cin>>n;int arr[n];sum=0;
+		for(int i=0;i<n;i++)
+		{
+			cin>>arr[i];sum+=arr[i];
+		}
+		sum=-sum;
+		// inverting the array.
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=-arr[i];
+		}
+
+		// finding max submarray sum for inverted array.
+		int csum=arr[0],maxs=arr[0];
+		for(int i=0;i<n;i++)
+		{
+			csum+=arr[i];
+			if(csum<0){
+				csum=0;
+			}
+			maxs=max(csum,maxs);
+		}
+
+
+		cout<<(-(sum-maxs))<<"\n";
+	}
+	return 0;
+}
