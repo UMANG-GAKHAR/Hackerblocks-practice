e13493a7af739f89ca2f622df7ea5c4ee454a5b2
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-07-24 10:58:10 +0530

    Create Form Biggest Number.cpp

---
+++ Form Biggest Number.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int             unsigned long long
+#define w(x)            int x; cin>>x; while(x--)
+
+
+void sm()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+bool cmpr(string a, string b) {
+   return a+b > b+a;
+}
+
+int32_t main()
+{
+	sm();
+	w(t) {
+		int n;
+		cin >> n;
+		string a[n];
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+		}
+		sort(a, a + n,cmpr);
+		for (int i = 0; i < n; i++) {
+			cout << a[i];
+		}
+		cout << endl;
+	}
+	return 0;
+}
