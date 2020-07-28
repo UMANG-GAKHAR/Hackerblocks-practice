1d7c7bac9fd8d5ec27b058d01187642e0fe4b7b9
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-07-28 10:12:09 +0530

    Create Exchanging Coins.cpp

---
+++ Exchanging Coins.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<unordered_map>
+#define ll long long
+using namespace std;
+ll int solve(ll int n,unordered_map<ll int,ll int>&dp)
+{
+	if(dp.count(n))
+		return dp[n];
+	if(n<12)
+	{
+		dp[n]=n;
+		return dp[n];
+	}
+
+		dp[n]=max(n,solve(n/2,dp)+solve(n/3,dp)+solve(n/4,dp));
+		return dp[n];
+	
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	unordered_map<ll int,ll int>dp;
+	cout<<solve(n,dp);
+}
