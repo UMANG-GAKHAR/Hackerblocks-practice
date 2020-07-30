36d082abd6f5f0c178cc1917fdeee98a25696b63
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-07-30 10:36:32 +0530

    Create Activity Selection Problem.cpp

---
+++ Activity Selection Problem.cpp
@@ -0,0 +1,32 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+bool mycompare(pair<int,int>a , pair<int,int>b){
+	return a.second<b.second;	
+}
+
+int main() {
+	int t;cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		vector<pair<int,int>>v;
+		for(int i=0;i<n;i++){
+			int x,y;
+			cin>>x>>y;
+			v.push_back({x,y});
+		}
+		sort(v.begin(),v.end(),mycompare);
+		int Time_elapsed=0;
+		int cnt=0;
+		for(auto it:v){
+			if(it.first>=Time_elapsed){
+				cnt++;
+				Time_elapsed=it.second;
+			}
+		}
+		cout<<cnt<<endl;
+	}
+	return 0;
+}
