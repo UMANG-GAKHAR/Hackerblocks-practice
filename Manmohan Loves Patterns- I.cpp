45cc0fab017252662d8ce37a922c64d7ae564dbb
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-08-12 11:14:44 +0530

    Create Manmohan Loves Patterns- I.cpp

---
+++ Manmohan Loves Patterns- I.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+     int n;
+     cin>>n; 
+     cout << 1 << "\n";
+     for(int i=2;i<=n;i++) { 
+         cout << 1;
+         for(int j=1;j<=i-2;j++) {
+              if(i%2==0) 
+                  cout<<0; 
+             else 
+                 cout<<1;
+          }
+         cout << 1 <<"\n";
+     }
+        return 0;
+ }
