bb2f52aa0e3197cc20cd3f921165e56d51b9b0aa
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-08-16 20:14:55 +0530

    Create Strings-Max Frequency Character.cpp

---
+++ Strings-Max Frequency Character.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{     
+    int maxm=-1;
+    char res;
+    char freq[256]={0};
+	char str[1000];
+
+	cin>>str;
+	for(int i=0;str[i]!='\0';i++)
+	{
+		freq[str[i]]++;
+		
+	}
+	for(int i=0;str[i]!='\0';i++)
+	{
+		if(maxm < freq[str[i]])
+		{
+            maxm = freq[str[i]];
+			res=str[i];
+			
+		}
+	}
+	cout<<res<<endl;
+	
+	return 0;
+}
+
