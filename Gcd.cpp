94b32361e4be428098601eecaaf9ade3e573e509
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-08-19 10:27:37 +0530

    Create Gcd.cpp

---
+++ Gcd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int gcd(int a,int b){
+    if(b==0){
+        return a;
+    }
+    return gcd(b,a%b);
+	
+}
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<gcd(a,b);
+}
