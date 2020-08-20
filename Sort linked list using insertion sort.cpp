aa5fb72c56be44c013b1a3560cd60d02a8e3a250
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-08-20 21:10:30 +0530

    Create Sort linked list using insertion sort.cpp

---
+++ Sort linked list using insertion sort.cpp
@@ -0,0 +1,77 @@
+// insertion sort
+#include<iostream>
+using namespace std;
+class node{
+    public: int data;
+            node* next;
+            node(int data)
+            {
+                this->data=data;
+                next=NULL;
+            }
+};
+void inserAtTail(node* &head,int value)
+{
+    node* temp=new node(value);
+    if(head==NULL)
+     head=temp;
+     else
+     {
+         node* trav=head;
+         while(trav->next!=NULL)
+         {
+             trav=trav->next;
+         }
+         trav->next=temp;
+     }
+}
+void print(node* head)
+{
+    while(head!=NULL)
+    {
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+}
+void InsertionSort(node* &head)
+{
+    if(head->next==NULL)
+     return;
+    node* front=head->next->next;
+    node* curr=head->next;
+    head->next=NULL;
+    while(curr!=NULL)
+    {
+        node* temp=head;
+        node* prev=NULL;
+        while(temp->data<curr->data && temp!=NULL)
+        {
+            prev=temp;
+            temp=temp->next;
+        }
+        if(prev==NULL) // condition for insertion at head
+         {
+             head=curr;
+         }
+         else{   // condition for insertion at middle and end
+             prev->next=curr;
+         }
+         curr->next=temp;
+         curr=front;
+         front=front->next;
+    }
+}
+int main()
+{
+    node* head=NULL;
+    int n,value;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>value;
+        inserAtTail(head,value);
+    }
+    InsertionSort(head);
+    print(head);
+    return 0;
+}
