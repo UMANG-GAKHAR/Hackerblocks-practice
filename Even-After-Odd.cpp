8dab4174deac1052253a6c945e3474ec64e90d7a
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-08-20 21:12:22 +0530

    Create Even-After-Odd.cpp

---
+++ Even-After-Odd.cpp
@@ -0,0 +1,160 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node 
+{
+    public:
+    int data;
+    node*next;
+    node(int d) 
+    {
+        data = d;
+        next = NULL;
+    }
+};
+void insertattail(node*&head, int d) 
+{
+    if(head==NULL)
+    {
+		head = new node(d);
+        return;
+	}
+	//else{
+		node*temp = head;
+		while(temp->next!=NULL)
+        {
+			temp = temp->next;
+		}
+		temp->next = new node(d);
+        return;
+}
+
+void evenodd(node*head)                                 //No need to return node
+{
+    node *es=NULL;
+    node *ee=NULL;
+    node *os=NULL;
+    node *oe=NULL;
+    node *curr=head;                                     //ADDED
+
+    //for(node*cur = head;cur!=NULL;cur=cur->next) 
+    while(curr!=NULL)                                   //ADDED
+    {
+        if(curr->data%2==0) 
+        {
+            if(es==NULL)
+            {
+              es = curr;
+              ee = curr;                                //Changed
+            }
+            else
+            {
+             ee->next = curr;
+             ee = ee->next;
+            }
+        }
+        else
+        {
+            if(os==NULL)
+            {
+              os = curr;
+              oe = curr;                            //Changed
+            }
+            else
+            {
+             oe->next = curr;
+             oe = oe->next;
+            }
+        }
+        curr=curr->next;                                    //Added
+    }
+
+    if(os==NULL || es==NULL)                                //Changed
+    {
+        return;             
+    }
+    //os->next = os;
+    oe->next = es;                                          //Changed
+    ee->next=NULL;                                          //Changed
+    head=os;                                                //Changed
+}
+
+/*node* reverseg(node*head, int k) {
+    node* current = head;  
+    node* next ;  
+    node* prev = NULL;  
+    int count = 0;  
+      
+    while (current != NULL && count < k)  
+    {  
+        next = current->next;  
+        current->next = prev;  
+        prev = current;  
+        current = next;  
+        count++;  
+    }  
+      
+    if(next != NULL)  {
+    head->next = reverseg(next, k);
+    }  
+
+  return prev;   
+}
+*/
+/*void remove(node*&head) {
+ if(head==NULL){
+     return;
+ }
+ node*s = head;
+ node*f = head;
+ while(f!=NULL && f->next!=NULL){
+     s = s->next;
+     f = f->next->next;
+ }
+ if(s!=f){
+     return;
+ }
+ s = head;
+ while(s->next!=f->next) {
+     s = s->next;
+     f = f->next;
+ }
+ f->next = NULL;
+}
+*/
+void buildlist(node *&head)
+{
+  int N;
+  cin>>N;
+  int data;
+  while(N>0)
+  {
+    cin>>data;
+    insertattail(head,data);
+    N--;
+  }
+}
+void print(node*head) 
+{
+    //if(head==NULL){
+      //  return;
+    //}
+    node*temp=head;
+    while(temp!=NULL) 
+    {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
+int main() 
+{
+  node*head = NULL;  
+  //int n;
+  //cin>>n;
+  //for(int i=0;i<n;i++){
+    //  int d;
+     // cin>>d;
+      //insertattail(head,d);
+   buildlist(head);                         //Added function to build the linked list
+  evenodd(head);
+ print(head);
+}
