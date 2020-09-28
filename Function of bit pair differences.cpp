336c965bb9b6546c90db554decfd05c0fe386090
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-09-28 19:14:26 +0530

    Create Function of bit pair differences.cpp

---
+++ Function of bit pair differences.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+#define ll long long
+
+ll sumBitDifferences(ll a[], ll n) { 
+    ll ans = 0; 
+    for (ll i=0; i<32;i++) { 
+        ll count=0; 
+        for (ll j=0; j<n; j++) {
+            if ((a[j] &(1<<i))) {
+                count++; 
+            }
+        }
+        ans += (count*(n-count)*2); 
+    } 
+    return ans; 
+} 
+
+int main() {
+       ll t;
+       cin >> t;
+    while(t--) {
+        ll n;
+        cin >> n;
+        ll a[n];
+        for(ll i=0;i<n;++i) {
+            cin >> a[i];
+        }
+        cout << sumBitDifferences(a,n) << "\n";
+    } 
+    return 0; 
+} 
+
