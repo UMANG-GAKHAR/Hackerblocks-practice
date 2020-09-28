2da4e27bbde151c9f02a341169d91d69a2573da7
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-09-28 10:11:45 +0530

    Create Job for Bountie.cpp

---
+++ Job for Bountie.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define mod 1000000007
+#define pp pair<ll,ll>
+#define mp make_pair
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+using namespace std;
+
+int main(){
+//    freopen("input2.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+    ios::sync_with_stdio(0);
+
+    string str;
+    stack<int> stck;
+    int dp[100010] = {0},ans=0;
+    //dp[i] stores the length of longest valid substring if it teerminates at ith position
+
+    cin>>str;
+    for(int i=0;i<str.length();i++){
+        if(str[i] == '(')
+            stck.push(i);
+        else if(!stck.empty()){
+            dp[i] = i-stck.top()+1;//length of open and close bracket pair
+            if(stck.top() > 0)
+                dp[i] += dp[stck.top()-1];//add length of previous valid substrings
+            ans = max(ans,dp[i]);//update ans if value of dp[i] is more than ans
+            stck.pop();//pop '(' from stack
+        }
+    }
+
+    cout<<ans;
+
+    return 0;
+}
