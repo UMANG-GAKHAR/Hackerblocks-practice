b2b3c98a1c0f23099d776af90222e42c9020dd9e
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-10-02 19:34:38 +0530

    Create All Indices Problems.cpp

---
+++ All Indices Problems.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> allIndices(const vector<int> &v, int key, int i, int idx)
+{
+    if (i == v.size())
+    {
+        return vector<int>(idx);
+    }
+
+    if (v[i] == key)
+    {
+        vector<int> remainingAns = allIndices(v, key, i + 1, idx + 1);
+        remainingAns[idx] = i;
+        return remainingAns;
+    }
+    else
+    {
+        return allIndices(v, key, i + 1, idx);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    int key;
+    cin >> key;
+
+    vector<int> ans = allIndices(v, key, 0, 0);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+
+    return 0;
+}
