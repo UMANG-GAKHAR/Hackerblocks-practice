c8cecfbe49d563779c7a9db815797d10e5adbea5
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-10-03 18:27:13 +0530

    Create Class Assignments.cpp

---
+++ Class Assignments.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long int fib(long long int n)
+{
+    if (n <= 1)
+        return n;
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    int testCases;
+    cin >> testCases;
+    int k = 1;
+
+    while (testCases--)
+    {
+        int n;
+        cin >> n;
+        cout << "#" << k++ << " : " << fib(n+2) << endl;
+    }
+
+    return 0;
+}
