523ee6a6fd135ab9581f567980d275bd12f8b305
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-10-12 15:08:36 +0530

    Create Smart Keypad - II.cpp

---
+++ Smart Keypad - II.cpp
@@ -0,0 +1,30 @@
+  
+#include <iostream>
+#include <cstring>
+using namespace std;
+string table[] = {" ", ".+@$", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+void phoneKeypad(string inp, string out)
+{
+    //Base case
+    if (inp.size() == 0)
+    {
+        cout << out << "\n";
+        return;
+    }
+
+    //Rec case
+    int r = inp[0] - '0';
+    for (unsigned int k = 0; k < table[r].length(); k++)
+    {
+        char ch = table[r][k];
+        phoneKeypad(inp.substr(1), out + ch);
+    }
+}
+int main()
+{
+    string input;
+    cin >> input;
+
+    phoneKeypad(input, "");
+    return 0;
+}
