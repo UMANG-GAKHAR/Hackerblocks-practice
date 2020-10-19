d24dc8929c7a132fc4f6b9820f9f612fdfeafcb9
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2020-10-19 20:23:54 +0530

    Create Average of Sliding Windows.cpp

---
+++ Average of Sliding Windows.cpp
@@ -0,0 +1,44 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class MovingAverage {
+public:
+    /** Initialize your data structure here. */
+    queue<int> q;
+    int ms;
+    int sum;
+
+    MovingAverage(int size) {
+        ms=size;
+        sum=0;
+    }
+
+    double next(int val) {
+        q.push(val);
+        sum+=val;
+        if(q.size()>ms){
+            sum-=q.front();
+            q.pop();
+        }
+        double ans=(double)(sum)/q.size();
+        return ans;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    int ws;
+    cin>>ws;
+    MovingAverage odj(ws);
+    int num;
+    cin>>num;
+    while(num!=-1)
+    {
+        cout<<odj.next(num)<<" ";
+        cin>>num;
+    }
+
+    return 0;
+}
+
