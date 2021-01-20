6b5bf58757fad60dcd5c10b75b019408bd09af99
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2021-01-20 19:02:46 +0530

    Update String Sort.cpp

--- String Sort.cpp
+++ String Sort.cpp
@@ -1,6 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 bool comp(string a,string b){
 if (a.size()<=b.size()&& b.substr(0,a.size())==a) return false;
 else if (b.size()<=a.size() && a.substr(0,b.size())==b) return true;
