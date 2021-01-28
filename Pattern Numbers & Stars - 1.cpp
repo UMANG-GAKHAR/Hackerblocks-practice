de28ff99c5163c9f770f5a15d5d2f686dafef939
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2021-01-28 21:49:33 +0530

    Update Pattern Numbers & Stars - 1.cpp

--- Pattern Numbers & Stars - 1.cpp
+++ Pattern Numbers & Stars - 1.cpp
@@ -4,14 +4,12 @@ int main ()
 {
     int i, j, n;
     cin >> n;
-
     // loop for each row
     for (i = 1; i <= n; i++)
     {
        // work for numbers 
         for (j = 1; j <= n+1-i; j++)
             cout << j << " ";
-
        // work for stars
         for (j = 1; j <= 2*(i-1) - 1; j++)
             cout << "* ";
