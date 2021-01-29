00059fd2f132373248110a10ddba638d2db8fe70
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2021-01-29 18:57:19 +0530

    Update LCM.cpp

--- LCM.cpp
+++ LCM.cpp
@@ -3,13 +3,11 @@ using namespace std;
 int main() {
     int num1,num2;
     cin >> num1>>num2;
-
     if(num1>num2){
         int temp = num2;
         num2=num1;
         num1=temp;
     }
-
     int lcm=num2;
     while(lcm>0){
         if(lcm%num2==0 && lcm%num1==0){
