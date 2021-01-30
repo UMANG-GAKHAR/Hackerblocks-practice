db00b595d68c019c6b8c5c9aafbf9ca1f90a3f19
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2021-01-30 19:38:08 +0530

    Update Tricky Permutations.cpp

--- Tricky Permutations.cpp
+++ Tricky Permutations.cpp
@@ -1,18 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 void permutationNoDuplicates(string ques, string ans)
 {
-
     if (ques.length() == 0)
     {
         cout << ans << "\n";
         return;
     }
-
     for (int i = 0; i < ques.length(); i++)
     {
-
         char ch = ques[i];
         string ros = ques.substr(0, i) + ques.substr(i + 1);
 
@@ -30,15 +26,11 @@ void permutationNoDuplicates(string ques, string ans)
             permutationNoDuplicates(ros, ans + ch);
     }
 }
-
 int main()
 {
     string str;
     cin >> str;
-
     sort(str.begin(), str.end());
-
     permutationNoDuplicates(str, "");
-
     return 0;
  }
