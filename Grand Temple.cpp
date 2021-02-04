9159d0a360ab5501cba5f598f8aea3785fd2c320
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2021-02-04 21:44:02 +0530

    Update Grand Temple.cpp

--- Grand Temple.cpp
+++ Grand Temple.cpp
@@ -13,11 +13,9 @@ int main() {
     for(ll i=0;i<n;i++){
         ll a,b;
         cin >> a >> b;
-
         x.push_back(a);
         y.push_back(b);
     }
-
     sort(x.begin(),x.end());
     sort(y.begin(),y.end());
 
@@ -26,17 +24,12 @@ int main() {
         ll diff = x[i] - x[i-1] - 1 ;
         deltaX = max(deltaX,diff);
     }
-
     ll deltaY = 0;
     for(ll i=1;i<y.size();i++){
         ll diff = y[i] - y[i-1] - 1 ;
         deltaY = max(deltaY,diff);
     }
-
-    
     ll maxArea = deltaX * deltaY ;
     cout << maxArea ;
-
-
     return 0;
 }
