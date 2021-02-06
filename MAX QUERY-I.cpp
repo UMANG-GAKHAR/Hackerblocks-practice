b749b551e77b1e38220c691dc87aa575c56cd3b8
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2021-02-06 19:09:39 +0530

    Update MAX QUERY-I.cpp

--- MAX QUERY-I.cpp
+++ MAX QUERY-I.cpp
@@ -13,15 +13,11 @@ ll query(vector<vector<ll>> &tree,int ss,int se,int qs,int qe,int index,ll k)
 
         return tree[index].end()-lower_bound(tree[index].begin(),tree[index].end(),k);  
     }
-
-
     int mid = (ss+se)/2;
     ll leftAns = query(tree,ss,mid,qs,qe,2*index,k);
     ll rightAns  = query(tree,mid+1,se,qs,qe,2*index+1,k);
     return  (leftAns + rightAns);
 }
-
-
 void BuildTree(vector<vector<ll>> &tree,vector<ll> &arr,int s,int e,int index)
 {
     if(s==e)
@@ -29,8 +25,6 @@ void BuildTree(vector<vector<ll>> &tree,vector<ll> &arr,int s,int e,int index)
         tree[index].push_back(arr[s]);
         return;
     }
-
-
     int mid=(s+e)/2;
     BuildTree(tree,arr,s,mid,index*2);
     BuildTree(tree,arr,mid+1,e,index*2+1);
