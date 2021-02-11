d069964fbc47dd24929a2aa04ab24e55829998e8
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2021-02-11 18:18:42 +0530

    Update Arrays-Target Sum Pairs.cpp

--- Arrays-Target Sum Pairs.cpp
+++ Arrays-Target Sum Pairs.cpp
@@ -1,13 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int main() 
 {
-	int n;
+     int n;
     cin>>n;
-
     int a[n];
-
     for(int i=0;i<n;i++)
     cin>>a[i];
 
@@ -15,7 +12,6 @@ int main()
     cin>>target;
 
 	sort(a,a+n);          //sort the array
-
     int l=0, r=n-1;        // l point to starting element,  r points to last element
 
     while(l<r)            // traverse whole array
