c29a0d994d57ffd10883929cc72393cb9733ce28
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2021-02-11 18:15:31 +0530

    Update Arrays-Insertion Sort.cpp

--- Arrays-Insertion Sort.cpp
+++ Arrays-Insertion Sort.cpp
@@ -10,6 +10,7 @@ for (i = 1; i < n; i++)
 {
     key = arr[i];
     j = i-1;
+
     /* Move elements of arr[0..i-1], that are
         greater than key, to one position ahead
         of their current position */
@@ -21,6 +22,7 @@ for (i = 1; i < n; i++)
     arr[j+1] = key;
 }
 }
+
 // A utility function to print an array of size n
 void printArray(long long int arr[],long long int n)
 {
@@ -29,15 +31,19 @@ for (i=0; i < n; i++)
     printf("%d ", arr[i]);
 printf("\n");
 }
+
 /* Driver program to test insertion sort */
 int main()
 {
+
     long long int n;
     cin>>n;
     long long int arr[n];
     for (int i = 0; i < n; i++)
         cin>>arr[i];
+
     insertionSort(arr, n);
     printArray(arr, n);
+
     return 0;
 }
