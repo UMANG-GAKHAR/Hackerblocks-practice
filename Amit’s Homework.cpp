2eea96e8648e90b51292661545bd43f88d0d3446
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2021-02-22 20:25:30 +0530

    Update Amit’s Homework.cpp

--- Amit’s Homework.cpp
+++ Amit’s Homework.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define MOD 1000000007
 
@@ -12,8 +11,6 @@ ll gcd(ll a,ll b){ if(a==0) {x=0,y=1; return b;}ll gc=gcd(b%a,a);ll temp;temp=x;
 
 //(a^x)%m
 ll fastPow(ll a, ll x,ll m=MOD){ if(x==0){return 1;}ll ans=1;ll k=1;  while(k<=x) {if(x&k){ans=((ans*a)%m);} k<<=1; a*=a; a%=m; }return ans; } 
-
-
 int main() {
     sync;
 
