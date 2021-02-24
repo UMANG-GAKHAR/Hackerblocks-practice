9e3f12f6eb4b3d8b24aeea30094218c8f0b84e10
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2021-02-24 19:37:37 +0530

    Update Optimal Game Strategy-I.cpp

--- Optimal Game Strategy-I.cpp
+++ Optimal Game Strategy-I.cpp
@@ -6,8 +6,6 @@ ll optimalGame(ll i,ll j){
     if(i > j){
         return 0;
     }
-    // Consider both the possibilities. You can pick either the first or the last coin.
-    // Since the opponent plays optimally , we would get the minimum of the remaining coins for each choice.
     ll pickFirst = coins[i] + min( optimalGame(i+2,j) , optimalGame(i+1,j-1) ) ;
     ll pickLast = coins[j] + min( optimalGame(i,j-2) , optimalGame(i+1,j-1) ) ;
     // Pick the max of two as your final result
