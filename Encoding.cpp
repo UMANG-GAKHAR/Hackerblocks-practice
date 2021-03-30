1b24319e9bb3735184de2c21435189b6700e665f
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2021-03-30 18:47:36 +0530

    Update Encoding.cpp

--- Encoding.cpp
+++ Encoding.cpp
@@ -19,7 +19,6 @@
 #define rep(i, n) for (int i = 0; i < (n); i++)
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
-
 using namespace std;
 using ll = long long;
 using ld = long double;
