882a055b8d08786d70454d53d441c5b9c45bb600
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2021-04-23 19:54:02 +0530

    Update Arrays-Binary Search.cpp

--- Arrays-Binary Search.cpp
+++ Arrays-Binary Search.cpp
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include<iostream>
 using namespace std;
-// A iterative binary search function. It returns
-// location of x in given array arr[l..r] if present,
-// otherwise -1
 int binarySearch(int arr[], int l, int r, int x)
 {
     while (l <= r)
     {
         int m = l + (r-l)/2;
-
         // Check if x is present at mid
         if (arr[m] == x)
             return m;
@@ -22,12 +18,8 @@ int binarySearch(int arr[], int l, int r, int x)
         else
             r = m - 1;
     }
-
-    // if we reach here, then element was
-    // not present
     return -1;
 }
-
 int main(void)
 {
     int n;
