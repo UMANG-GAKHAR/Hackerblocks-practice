56e15cd5d30cbca8a8b754d63de1febc00b2ae41
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2021-04-23 19:57:31 +0530

    Update Strings-Toggle Case.cpp

--- Strings-Toggle Case.cpp
+++ Strings-Toggle Case.cpp
@@ -3,7 +3,6 @@ using namespace std;
 int main() {
     char str[1001];
     cin >> str;
-
     for (int i = 0; str[i]!='\0'; i++) {
         if(str[i]>='A'&&str[i]<='Z'){
             str[i]=str[i]+32;
