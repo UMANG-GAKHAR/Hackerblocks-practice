e24510b75558468936cf9d8de84012077cef6859
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2021-05-05 20:16:26 +0530

    Update Replace Them All.cpp

--- Replace Them All.cpp
+++ Replace Them All.cpp
@@ -7,7 +7,7 @@ int main ()
     long long int sum = 0, pow = 1, t;
     do
     {
-        t = n%10;        // Extracting the digit
+        t = n%10;        
         if (t == 0)      
             t = 5;       // If digit is zero then make t = 5
         sum = sum + t*pow;   // Prepare the answer
