ee91b67ca14923a261ae994fb6576c4b842aaff7
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2021-05-09 19:53:43 +0530

    Update Longest Increasing subsequence.cpp

--- Longest Increasing subsequence.cpp
+++ Longest Increasing subsequence.cpp
@@ -19,7 +19,6 @@ int main()
 int n; cin>>n;
     int arr[n] ; 
 for(int i=0;i<n;i++) cin>>arr[i];
-  // int n = sizeof(arr)/sizeof(arr[0]); 
     printf("%d", 
            lis( arr, n )); 
     return 0; 
