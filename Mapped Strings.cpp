d867ecc6cb56b488ddbff0e55e07d77cca0119b6
Umang Gakhar <37589983+UMANG-GAKHAR@users.noreply.github.com>
2021-05-12 18:13:45 +0530

    Update Mapped Strings.cpp

--- Mapped Strings.cpp
+++ Mapped Strings.cpp
@@ -17,8 +17,6 @@ void mappedStrings(string inp, string out)
         int doubleDigitNumber = (inp[0] - '0') * 10 + (inp[1] - '0');
         if (doubleDigitNumber > 9 && doubleDigitNumber <= 26)
         {
-            //If doubleDigitNumber is a double digit number i.e. does not have a 0 at 10s
-            //place and less than 26 (number over 26 has no letter corresponding to it)
             ch = doubleDigitNumber + 'A' - 1;
             mappedStrings(inp.substr(2), out + ch);
         }
