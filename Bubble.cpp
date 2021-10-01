3b51cd39616cc4717e42684acfed4869b0425c37
riya9888 <91748431+riya9888@users.noreply.github.com>
2021-10-01 21:29:51 +0530

    Create Bubble.cpp

---
+++ Bubble.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+    long long int n;
+    cin >> n;
+    long long int i,j,k;
+    long long int arr[n];
+    //Input from the user
+    for (i = 0; i < n; i++)
+        cin >> arr[i];
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n-1;j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                k = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = k;
+            }
+        }
+    }
+    //Print required Output
+    for (i = 0; i < n; i++)
+        cout << arr[i] << endl;
+    return 0;
+}
